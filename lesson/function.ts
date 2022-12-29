2cf51ba1e2a558529a55efa974b1772b8ee2725d
thanghuyht <ht.huy.thang@gmail.com>
2022-12-29 12:49:34 +0700

    hoc typescrip

---
+++ lesson/function.ts
@@ -0,0 +1,36 @@
+
+
+// const ahihi = (a: string) => console.log(a)
+
+// ahihi('aaa')
+
+// const sum = (a: number, b: number): void => console.log(a + b);
+
+// sum(7, 8)
+
+//----- number
+// function sum(a: number, b: number): number {
+//     return a + b
+// }
+// console.log(sum(4, 3));
+
+// ----- void type ko return gi ca
+// function sum(a: number, b: number): void {
+//     console.log(a + b);
+// }
+// sum(4, 6)
+
+//----- string
+// function sum(a: number, b: number): string {
+//     return `${a} + ${b}`
+// }
+// console.log(sum(4, 3))
+
+//initia lizer
+// function showArr(a: string, b: number = 4) {
+//     let newArr: string[] = []
+//     newArr.push(a)
+//     newArr.push(`${b}`)
+//     return newArr
+// }
+// console.log(showArr("a"));
